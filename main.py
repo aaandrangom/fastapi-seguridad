561b1c3823e79979e7ff4ac64fc95c8b79b9cbf4
Anthony Andrango <aaandrangom@utn.edu.ec>
2023-07-20 19:16:46 -0500

    Test connection

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ app.include_router(token_routes)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
